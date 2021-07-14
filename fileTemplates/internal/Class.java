86cb6741be88c64e2e8e7e6fa3da6690c29728f5
chjiyun <lijiyun615@163.com>
2021-07-14 15:45:02 +0800

    IU-2021.1.2 <chjiyun@yuanbao-chjiyun Create baseRefactoring.xml, ide.general.xml, mybatisx.xml, ide-features-trainer.xml, project.default.xml, mybatisMapper.xml, visualizationTool.xml, find.xml, one_dark_config.xml, ui.lnf.xml, filetypes.xml, projectView.xml, keymap.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, templates.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, presentation-assistant.xml, wsl.distributions.xml, chjiyun.xml, vcs.xml, databaseSettings.xml, editor.xml, certificates.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * 
+ * @author lijiyun
+ * @date ${YEAR}年${MONTH}月${DAY}日 ${TIME}
+ */
+public class ${NAME} {
+}
