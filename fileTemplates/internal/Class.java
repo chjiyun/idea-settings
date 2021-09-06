f437af6f91b482457e73a4b7cb6b3433dbfe67f4
chjiyun <lijiyun615@163.com>
2021-09-06 10:28:35 +0800

    IU-2021.1.2 <chjiyun@yuanbao-chjiyun Overwrite remote https://github.com/chjiyun/idea-settings to local

--- fileTemplates/internal/Class.java
+++
@@ -1,9 +0,0 @@
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-#parse("File Header.java")
-/**
- * 
- * @author lijiyun
- * @date ${YEAR}年${MONTH}月${DAY}日 ${TIME}
- */
-public class ${NAME} {
-}
